b32adb1da0cddf7edb5ecf8c6056b8b5bf1c99a3
Georgi Georgiev <georgiev.georgi.st@gmail.com>
2018-05-22 18:10:24 +0300

    update

--- swr/multiplot.plt
+++ swr/multiplot.plt
@@ -4,33 +4,19 @@ set term pdf size 12,4
 
 #FF=system("echo $FILE")
 FF=system("ls *.swr|paste -sd ' ' ")
+TITLE=system("basename $PWD")
 #FF="tmp.dat"
 set o sprintf("%s.pdf",FF)
 PLOT="p"
 do for [f in FF] {
-  PLOT=sprintf("%s '%s' u ($1/1000.):2 w l,",PLOT,f)
+  PLOT=sprintf("%s '%s' u ($1/1000.):2 w lp ps .5 pt 2 tit '%s',",PLOT,f,f)
 }
 PLOT=PLOT."1 notit lt 0"
 
 
-bm = 0.19
-tm = 0.75
-lm = 0.06
-rm = 0.97
-gap = 0.01
-size = 0.81/12
 
-
-
-
-
-
-set multiplot tit FF
+set multiplot tit TITLE layout 1,12 margins 0.06,.98,0.1,0.83 spacing 0.01;
 set xtics nomirror
-set lmargin at screen lm
-#set rmargin at screen rm
-set bmargin at screen bm
-set tmargin at screen tm
 set grid
 band_i=0
 
@@ -42,27 +28,26 @@ set xla "Freq [MHz]"
 set yrange [.8:8]
 set ytics format "%4.1f"
 
-set xtics rotate by 60 autofreq font ",8" offset 0,0 right format "%5.2f"
+set xtics rotate by 60 font ",8" offset 0,0 right format "%5.2f"
 
-FRST="set border 1+4+2;unset key; set yla 'SWR'; set ytics (1,1.5,2,3,5)"
-NEXT="set border 1+4; unset ytics; set ytics  offset 10000 (1,1.5,2,3,5); unset yla "
-LAST="set border 1+4+8;set key"
-SHIFTPLOT="set lmargin at screen lm + size*band_i + gap*band_i;band_i=band_i+1;set rmargin at screen lm + size*band_i + gap*(band_i-1)"
-
+FRST="set border 1+4+2;unset key; set yla 'SWR'; set ytics (1,1.5,2,3,5);set xtics autofreq "
+NEXT="set border 1+4; set ytics  offset 10000; unset yla; set xtics autofreq "
+LAST="set border 1+4+8;set key; set xtics autofreq "
 
 
-set xrange [ 1.800: 2.000];set tit "160m";@FRST;@SHIFTPLOT;@PLOT
-set xrange [ 3.500: 3.800];set tit  "80m";@NEXT;@SHIFTPLOT;@PLOT
-set xrange [ 7.000: 7.200];set tit  "40m";@NEXT;@SHIFTPLOT;@PLOT
-set xrange [10.100:10.150];set tit  "30m";@NEXT;@SHIFTPLOT;@PLOT
-set xrange [14.000:14.350];set tit  "20m";@NEXT;@SHIFTPLOT;@PLOT
-set xrange [18.050:18.200];set tit  "17m";@NEXT;@SHIFTPLOT;@PLOT
-set xrange [21.000:21.450];set tit  "15m";@NEXT;@SHIFTPLOT;@PLOT
-set xrange [24.850:25.000];set tit  "12m";@NEXT;@SHIFTPLOT;@PLOT
-set xrange [28.000:30.000];set tit  "10m";@NEXT;@SHIFTPLOT;@PLOT
-set xrange [50.000:50.200];set tit   "6m";@NEXT;@SHIFTPLOT;@PLOT
-set xrange [144   :146   ];set tit   "2m";@NEXT;@SHIFTPLOT;@PLOT
-set xrange [430   :440   ];set tit "70cm";@LAST;@SHIFTPLOT;@PLOT
+
+set xrange [ 1.800: 2.000];set tit "160m";@FRST;set xtics 0.05;@PLOT ;
+set xrange [ 3.500: 3.800];set tit  "80m";@NEXT;@PLOT ;
+set xrange [ 7.000: 7.200];set tit  "40m";@NEXT;@PLOT ;
+set xrange [10.100:10.150];set tit  "30m";@NEXT;@PLOT ;
+set xrange [14.000:14.350];set tit  "20m";@NEXT;@PLOT ;
+set xrange [18.050:18.200];set tit  "17m";@NEXT;@PLOT ;
+set xrange [21.000:21.450];set tit  "15m";@NEXT;@PLOT ;
+set xrange [24.850:25.000];set tit  "12m";@NEXT;@PLOT ;
+set xrange [28.000:30.000];set tit  "10m";@NEXT;@PLOT ;
+set xrange [50.000:50.200];set tit   "6m";@NEXT;@PLOT ;
+set xrange [144   :146   ];set tit   "2m";@NEXT;@PLOT ;
+set xrange [430   :440   ];set tit "70cm";@LAST;@PLOT ;
 
 unset multiplot
 
--- swr/swr.py
+++ swr/swr.py
@@ -3,48 +3,61 @@ import serial
 import time
 import math
 import numpy
+import numpy as np
+
+
+power=5; ## watt
 
 
 port = serial.Serial("/dev/ttyUSB0", baudrate=4800, timeout=3.0)
+#port = serial.Serial("/dev/pts/7", baudrate=4800, timeout=3.0)
+
 rcv = port.read(999)
 
-port.write("PC005;")  ## set power to 5w
-port.write("MD03;")   ## set mode to 3:cw-usb 5:AM
+b160=numpy.linspace(  1810,  2000,10).astype(int)[1:-1]
+b80 =numpy.linspace(  3500,  3800,12).astype(int)[1:-1]
+b40 =numpy.linspace(  7000,  7200,10).astype(int)[1:-1]
+b30 =numpy.linspace( 10100, 10150,10).astype(int)[1:-1]
+b20 =numpy.linspace( 14000, 14350, 7).astype(int)[1:-1]
+b17 =numpy.linspace( 18068, 18168, 7).astype(int)[1:-1]
+b15 =numpy.linspace( 21000, 21450,10).astype(int)[1:-1]
+b12 =numpy.linspace( 24890, 24990, 6).astype(int)[1:-1]
+b10 =numpy.linspace( 28000, 29700,12).astype(int)[1:-1]
+b6  =numpy.linspace( 50050, 50200,12).astype(int)[1:-1]
+b2  =numpy.linspace(144000,146000,11).astype(int)[1:-1]
+b_70=numpy.linspace(430000,440000,21).astype(int)[1:-1]
 
-b80=range(3500000,3800000,30000)
-b40=range(7000000,7200000,20000)
-b30=range(10100000,10150000,10000)
-b20=range(14000000,14350000,30000)
-b17=range(18068000,18168000,10000)
-b15=range(21000000,21450000,50000)
-b12=range(24890000,24990000,50000)
-b10=range(28000000,29700000,100000)
+for band_i in [b80,b40,b20,b17,b15,b12,b10]:
+#for band_i in [b2,b_70]:
+    print("")
+    for freq_i in band_i:
+        print("#freq   "+str(freq_i)+"kHz")
 
-for i in (b80+b40+b20+b17+b15+b12+b10):
-    port.write("PC005;")  ## set power to 5w
-    port.write("MD03;")   ## set mode to 3:cw-usb 5:AM
-    cmd="FA%09d;" % (i)  ## Set Frequency in Hz
-    #print("#freq   "+str(i/1000)+"kHz")
-    port.write(cmd)
-    port.write("FA;") # get freq
-    rcv = port.read(12)
-    #print(rcv)
-    freqkhz=float(rcv[2:11])/1000
+        cmd="FA%09d;" % (freq_i*1000)  ## Set Frequency in Hz
+        port.write(cmd)
+        port.write("MD03;")   ## set mode to 3:cw-usb 5:AM
+        port.write("PC%03d;" % (power))  ## set power to 5w
+        port.write("AC000;")  ## switch off antenna tuner
+        port.write("FA;") # get freq
+        rcv = port.read(12)
+        #print(rcv)
+        freqkhz=float(rcv[2:11])/1000
 
-    time.sleep(.3)
-    port.write("TX1;") ## TX ON
-    swrarr=[]
+        time.sleep(.3)
+        swrarr=[]
+        port.write("TX1;") ## TX ON
 
-    for i in range(5):
-        time.sleep(.1)
-        port.write("RM6;") ## read SWR
-        rcv = port.read(7)
-        swrarr.append((math.log(342.483)-math.log(255-float(rcv[3:6])))/0.315294)
+        for i in range(5):
+            time.sleep(.1)
+            port.write("RM6;") ## read SWR
+            rcv = port.read(7)
+            swrarr.append(float(rcv[3:6]))
 
     port.write("TX0;") ## TX OFF
-    swr=numpy.array(swrarr).mean()
+    swrnat=numpy.array(swrarr).mean()
+    swr=(math.log(342.483)-math.log(256-swrnat))/0.315294
 
-    print(str(freqkhz)+"    "+str(swr))
+    print(str(freqkhz)+"    "+str(swr)+"     "+str(swrnat))
 
 
 
